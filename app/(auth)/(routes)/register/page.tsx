4abf7182e82685a658f1249a33057b3c66af0188
junyoung <no2jfamily@tukorea.ac.kr>
2023-12-28 19:54:16 +0900

    feat: Heading 컴포넌트 생성

--- app/(auth)/(routes)/register/page.tsx
+++
@@ -1,5 +0,0 @@
-const RegisterPage = () => {
-  return <div>Login page!</div>;
-};
-<div></div>;
-export default RegisterPage;
---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,10 @@
+"use Client";
+
+export const Heading = () => {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1>Your Ideas, Documents, & Plans</h1>
+      <span className="underline">tution</span>
+    </div>
+  );
+};
