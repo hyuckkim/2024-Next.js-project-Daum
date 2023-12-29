a6599f0a2c7e0868e97fc368ddf138a2d047a975
junyoung <no2jfamily@tukorea.ac.kr>
2023-12-29 13:10:19 +0900

    feat: Loading화면, 로그인 상태에 따른 navbar 화면 구현

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -8,7 +8,6 @@ export const Heading = () => {
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
         Your Ideas, Documents, & Plans. Unified. Welcome to
-        <br />
         <span className="underline">tution</span>
       </h1>
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://accurate-newt-5.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
