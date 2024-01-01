d94950a764ed622822ac64cc6db12555aa669d0d
junyoung <no2jfamily@tukorea.ac.kr>
2024-01-01 16:54:03 +0900

    build: cn 설치

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { Id } from "@/convex/_generated/dataModel";
-import { LucideIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
 
 interface ItemProps {
+  //"documents" : 테이블 이름을 의미
   id?: Id<"documents">;
   documentIcon?: string;
   active?: boolean;
@@ -28,12 +30,18 @@ export const Item = ({
   onExpand,
   expanded,
 }: ItemProps) => {
+  const ChevronIcon = expanded ? ChevronDown : ChevronRight;
+
   return (
     <div
       onClick={onClick}
       role="button"
-      style={{ paddingLeft: "12px" }}
-      className="group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium"
+      style={{
+        paddingLeft: level ? `${level * 12 + 12}px` : "12px",
+      }}
+      className={cn(
+        "group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium"
+      )}
     >
       <Icon
         className="shrink-0 h-[18px] mr-2 
