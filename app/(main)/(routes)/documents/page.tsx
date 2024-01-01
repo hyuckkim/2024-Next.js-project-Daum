3dadb196fcf676ae963c16e91146eb5b704e1a54
junyoung <no2jfamily@tukorea.ac.kr>
2024-01-01 13:25:15 +0900

    feat: useUser사용해 firstName 조회완료

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,31 @@
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+
 const DocumentsPage = () => {
-  return <div>This is a protected DocumentsPage</div>;
+  const { user } = useUser();
+
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      This is a protected DocumentsPage
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        height="300"
+        width="300"
+        alt="empty"
+        className="hidden dark:block"
+      />
+      <h2>Welcome to {user?.firstName}&apos;s tution</h2>
+    </div>
+  );
 };
 
 export default DocumentsPage;
