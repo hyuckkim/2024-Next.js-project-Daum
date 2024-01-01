795d01bb4d0519ceb40f37a3f817e1594ebbb6e7
junyoung <no2jfamily@tukorea.ac.kr>
2024-01-01 16:40:11 +0900

    feat: New page 클릭시 페이지 생성 구현 완료

---
+++ app/(main)/_components/item.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Id } from "@/convex/_generated/dataModel";
+import { LucideIcon } from "lucide-react";
+
+interface ItemProps {
+  id?: Id<"documents">;
+  documentIcon?: string;
+  active?: boolean;
+  expanded?: boolean;
+  isSearch?: boolean;
+  level?: number;
+  onExpand?: () => void;
+  label: string;
+  onClick?: () => void;
+  icon: LucideIcon;
+}
+
+export const Item = ({
+  id,
+  label,
+  onClick,
+  icon: Icon,
+  active,
+  documentIcon,
+  isSearch,
+  level = 0,
+  onExpand,
+  expanded,
+}: ItemProps) => {
+  return (
+    <div
+      onClick={onClick}
+      role="button"
+      style={{ paddingLeft: "12px" }}
+      className="group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium"
+    >
+      <Icon
+        className="shrink-0 h-[18px] mr-2 
+      text-muted-foreground"
+      />
+      <span>{label}</span>
+    </div>
+  );
+};
