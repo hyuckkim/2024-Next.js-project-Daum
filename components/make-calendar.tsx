cccc8b85e11eb7613f8228293e708a86167e499f
junyoung <no2jfamily@tukorea.ac.kr>
2024-01-26 14:29:29 +0900

    feat: calendar 기본 로직 구현

--- components/make-calendar.tsx
+++ components/make-calendar.tsx
@@ -17,20 +17,38 @@ import {
   isSaturday,
   isSunday,
 } from "date-fns";
+import { date } from "zod";
 
 const MakeCalendar = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
+  const [checkedDates, setCheckedDates] = useState<number[]>([]);
+
   const monthStart = startOfMonth(currentDate);
   const monthEnd = endOfMonth(currentDate);
   const startDate = startOfWeek(monthStart);
   const endDate = endOfWeek(monthEnd);
   const weekMock = ["일", "월", "화", "수", "목", "금", "토"];
+
   const nextMonthHandler = useCallback(() => {
     setCurrentDate(addMonths(currentDate, 1));
   }, [currentDate]);
+
+  const dateClick = (index: number) => {
+    // 클릭한 날짜의 index를 상태에 추가 또는 제거
+    setCheckedDates((prevCheckedDates) => {
+      const isDateChecked = prevCheckedDates.includes(index);
+      if (isDateChecked) {
+        return prevCheckedDates.filter((dateIndex) => dateIndex !== index);
+      } else {
+        return [...prevCheckedDates, index];
+      }
+    });
+  };
+
   const prevMonthHandler = useCallback(() => {
     setCurrentDate(subMonths(currentDate, 1));
   }, [currentDate]);
+
   const createMonth = useMemo(() => {
     const monthArray = [];
     let day = startDate;
@@ -40,6 +58,7 @@ const MakeCalendar = () => {
     }
     return monthArray;
   }, [startDate, endDate]);
+
   return (
     <section className={styles.calendar}>
       <div className={styles.yearTitle}>{format(currentDate, "yyyy년")}</div>
@@ -89,12 +108,14 @@ const MakeCalendar = () => {
           }
           return (
             <div
+              onClick={() => dateClick(i)}
               key={`date${i}`}
               className={validation ? styles.currentMonth : styles.diffMonth}
               style={style}
             >
               <div className={styles.topLine}>
                 <span className={styles.day}>{format(v, "d")}</span>
+                {checkedDates && <span>V</span>}
                 {today && <span className={styles.today}>(오늘)</span>}
               </div>
             </div>
