b14ae9c34b61abff39d89b58a15059e1be3b8a49
hyuckkim <monkeyhyuck@gmail.com>
2024-03-01 13:58:44 +0900

    feat: index 날짜로 바꾸는 함수 추가

--- types/calendar.ts
+++ types/calendar.ts
@@ -1,5 +1,4 @@
 import { Id } from "@/convex/_generated/dataModel";
-
 export type Calendar = CalendarDocumentElement[];
 
 export type CalendarDocumentElement = {
@@ -37,4 +36,26 @@ export function generateId() {
     return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
   };
   return `${S4()}${S4()}${S4()}${S4()}`;
+}
+
+export function parseDateByIndex(month: number, index: number): Date {
+  const today = new Date();
+
+  const thisYear = new Date(today.getFullYear(), month - 1, 1);
+  thisYear.setDate(thisYear.getDate() - thisYear.getDay() + index);
+
+  // 연도가 구분되어 있지 않고, '목표' 이므로 시간이 일주일을 넘기면 다음 해의 날짜를 대신 카운트
+  if (getDayDifference(today, thisYear) > -7) return thisYear;
+
+  const nextYear = new Date(today.getFullYear() + 1, month - 1, 1);
+  nextYear.setDate(nextYear.getDate() - nextYear.getDay() + index);
+
+  return nextYear;
+}
+
+function getDayDifference(date1: Date, date2: Date) {
+    const diffInMilliseconds = Math.abs(date2.getTime() - date1.getTime());
+    const millisecondsPerDay = 1000 * 60 * 60 * 24;
+    const daysDifference = Math.floor(diffInMilliseconds / millisecondsPerDay);
+    return daysDifference;
 }
