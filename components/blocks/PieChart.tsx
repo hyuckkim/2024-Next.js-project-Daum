7317a98f8984602e46e8845edb57afcbb5b41035
hyuckkim <monkeyhyuck@gmail.com>
2024-03-03 13:55:10 +0900

    feat: 차트와 표를 분리

--- components/blocks/PieChart.tsx
+++ components/blocks/PieChart.tsx
@@ -5,73 +5,6 @@ import {
 } from "@blocknote/core";
 import { ReactSlashMenuItem, createReactBlockSpec } from "@blocknote/react";
 import { MinusCircle, PieChart, PlusCircle } from "lucide-react";
-import React, { useEffect, useState } from "react";
-import Chart from "chart.js/auto";
-
-const PieChartComponent = ({
-  id, // 차트마다 id값
-  name,
-  tableData,
-  onChange,
-  onChartCreated,
-}: {
-  id: string; // 차트마다 id값
-  name: string;
-  tableData: string[][];
-  onChange: (name: string, tableData: string[][]) => void;
-  onChartCreated: () => void;
-}) => {
-  useEffect(() => {
-    // JSON 데이터를 파이 차트 데이터 형식으로 변환
-    const labels = tableData.map((row) => row[0]); // 첫 번째 열을 라벨로 사용
-    const data = tableData.map((row) => parseFloat(row[1])); // 두 번째 열을 데이터로 사용
-
-    // 차트 생성
-    const ctx = document.getElementById(`${id}_pieChart`) as HTMLCanvasElement; // ID 값 -> 캔버스 요소
-    let chart = new Chart(ctx, {
-      type: "pie",
-      data: {
-        labels: labels,
-        datasets: [
-          {
-            data: data,
-            backgroundColor: [
-              "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)",
-              "rgba(75, 192, 192, 0.2)",
-              "rgba(153, 102, 255, 0.2)",
-              "rgba(255, 159, 64, 0.2)",
-            ],
-            borderColor: [
-              "rgba(255, 99, 132, 1)",
-              "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
-              "rgba(75, 192, 192, 1)",
-              "rgba(153, 102, 255, 1)",
-              "rgba(255, 159, 64, 1)",
-            ],
-            borderWidth: 1,
-          },
-        ],
-      },
-    });
-
-    // 새로운 차트가 생성될 때 호출되는 콜백 함수 실행
-    onChartCreated();
-
-    // 컴포넌트가 언마운트되기 전에 차트 파기
-    return () => {
-      chart.destroy();
-    };
-  }, [id, tableData, onChartCreated]);
-
-  return (
-    <div style={{ width: "450px", height: "450px" }}>
-      <canvas id={`${id}_pieChart`} width="100%" height="100%"></canvas>
-    </div>
-  ); // 차트 ID값 사용-> 서로 다른 캔버스 생성 // 차트 ID값 사용-> 서로 다른 캔버스 생성
-};
 
 const getDefaultTableData = (): string[][] => [
   ["First Name", ""],
@@ -92,13 +25,6 @@ const RenderChartBlock = ({
   onChange: (name: string, tableData: string[][]) => void;
   onChartCreated: () => void;
 }) => {
-  const [chartCreated, setChartCreated] = useState(false);
-
-  const handleInputComplete = () => {
-    if (chartCreated) return;
-    setChartCreated(true);
-  };
-
   const addRow = () => {
     const newRow = ["", ""];
     onChange(name, [...tableData, newRow]);
@@ -126,9 +52,9 @@ const RenderChartBlock = ({
   return (
     <div>
       <div className="flex items-center">
-        <h1 style={{ fontWeight: "bold" }}>Pie Chart Table</h1>
+        <h1 style={{ fontWeight: "bold" }}>Chart Table</h1>
         <button
-          onClick={handleInputComplete}
+          onClick={onChartCreated}
           style={{
             borderRadius: "10px",
             backgroundColor: "#FFA979",
@@ -137,7 +63,7 @@ const RenderChartBlock = ({
             margin: "5px",
           }}
         >
-          차트 생성
+          차트로 변환
         </button>
       </div>
       <table id="chartTable">
@@ -162,15 +88,6 @@ const RenderChartBlock = ({
       >
         <PlusCircle/>
       </button>
-      {chartCreated && (
-        <PieChartComponent
-          id={id}
-          name={name}
-          tableData={tableData}
-          onChange={onChange}
-          onChartCreated={onChartCreated}
-        />
-      )}
     </div>
   );
 };
@@ -202,7 +119,11 @@ export const piechartBlock = createReactBlockSpec(
             });
           }}
           tableData={JSON.parse(block.props.tableData)}
-          onChartCreated={() => {}}
+          onChartCreated={() => {
+            editor.updateBlock(block, {
+              type: "piechart",
+            });
+          }}
         />
       );
     },
--- components/editor.tsx
+++ components/editor.tsx
@@ -14,6 +14,7 @@ import { CheckBoxBlockSpec, insertCheckBoxBlock } from "@/components/blocks/chec
 // import { chartBlock, insertChartBlock } from "./blocks/chart";
 import { insertPieChartBlock, piechartBlock } from "./blocks/PieChart";
 import { guestbookBlockSpec, insertGuestBookBlock } from "./blocks/guestbook";
+import { piechart } from "./blocks/PieChratComponent";
 
 interface EditorProps {
   onChange: (value: string) => void;
@@ -44,6 +45,7 @@ const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
     blockSpecs: {
       ...defaultBlockSpecs,
       checkboxListItem: CheckBoxBlockSpec,
+      piechart: piechart,
       chart: piechartBlock,
       guestbook: guestbookBlockSpec,
     },
